9615e3730737c7163969b89948e7118aabfd97a0
Vivek Dalal <dalal.vi@husky.neu.edu>
2019-03-22 21:09:25 -0400

    Added questions for the quiz

--- webapp/Quiz-Master/Models/QuestionManager.cs
+++ webapp/Quiz-Master/Models/QuestionManager.cs
@@ -13,20 +13,46 @@ namespace Quiz_Master.Models
 
             _questions = new List<Question> {
                 new Question() {
-                    question = "Select option A?",
-                    answers = new string[] { "Option A", "Option B", "Option C" },
-                    correctAnswer = 0
+                    question = "Agent 007, featured in many movies dating back to 1962, is known as _______ Bond?",
+                    answers = new string[] { "John", "Benedict", "James" },
+                    correctAnswer = 2
                 },
                 new Question() {
-                    question = "Select option B?",
-                    answers = new string[] { "Option A", "Option B", "Option C" },
+                    question = "What plant is traditionally the primary ingredient in wine?",
+                    answers = new string[] { "Agave", "Grape", "Peach" },
                     correctAnswer = 1
                 },
                 new Question() {
-                    question = "Select option C?",
-                    answers = new string[] { "Option A", "Option B", "Option C" },
+                    question = "The Average American does what 22 times a day?",
+                    answers = new string[] { "Opens Fridge", "Takes a bus", "Yawns" },
+                    correctAnswer = 0
+                },
+                new Question() {
+                    question = "In South Dakota it's illegal to fall down and sleep where?",
+                    answers = new string[] { "Class", "In a Cheese Factory", "Church" },
+                    correctAnswer = 1
+                },
+                new Question() {
+                    question = "It's illegal in Georgia to do what with a fork?",
+                    answers = new string[] { "Eat a cake", "Eat Spaghetti", "Eat fried chicken" },
                     correctAnswer = 2
+                },
+                new Question() {
+                    question = "Two fathers took their sons fishing. They all caught one fish each. When they came back, they had three fish. None of the fish was stolen, eaten or thrown overboard. How come they only had three?",
+                    answers = new string[] { "Because the grandfather took his son fishing, and the son took his son fishing", "Because someone ate it", "Because they gave one fish away" },
+                    correctAnswer = 0
+                },
+                new Question() {
+                    question = "What is the most common form of Chinese spoken in the south of China, in Hong Kong and in Macau?",
+                    answers = new string[] { "Shanghainese", "Hakka", "Cantonese" },
+                    correctAnswer = 2
+                },
+                new Question() {
+                    question = "What is Chinese called in Chinese?",
+                    answers = new string[] { "Zhong Yu", "Zhong Wen", "China Yu" },
+                    correctAnswer = 1
                 }
+
             };
         }
 
